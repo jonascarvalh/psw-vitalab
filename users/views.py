b10fd7398d339f54e1179c4cac551a8e4cfa79fe
jonascarvalh <olaffic@gmail.com>
2023-10-03 20:36:19 -0300

    create a view register

--- users/views.py
+++ users/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            return redirect('/users/register')
+        
+        if len(password) < 6:
+            return redirect('/users/register')
+        
+        try:
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
+        except:
+            return redirect('/users/register')
+        return HttpResponse('Passou!')
