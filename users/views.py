8154765ba0cde447ab44a87595ab3e9a9465ff9c
jonascarvalh <olaffic@gmail.com>
2023-10-03 22:25:29 -0300

    setup info messages

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-
+from django.contrib.messages import constants
+from django.contrib import messages
 # Create your views here.
 def register(request):
     if request.method == "GET":
@@ -15,9 +16,19 @@ def register(request):
         confirm_password = request.POST.get('confirm_password')
 
         if password != confirm_password:
+            messages.add_message(
+                request, 
+                constants.ERROR, 
+                "The passwords don't equals!"
+            )
             return redirect('/users/register')
         
         if len(password) < 6:
+            messages.add_message(
+                request, 
+                constants.ERROR, 
+                "Your passwords must be longer than 6 digits!"
+            )
             return redirect('/users/register')
         
         try:
@@ -28,6 +39,17 @@ def register(request):
                 email=email,
                 password=password,
             )
+            messages.add_message(
+                request, 
+                constants.SUCCESS, 
+                "Account registered!"
+            )
         except:
+            messages.add_message(
+                request, 
+                constants.ERROR, 
+                "Error in our system!"
+            )
             return redirect('/users/register')
-        return HttpResponse('Passou!')
+        
+        return redirect('/users/register')
